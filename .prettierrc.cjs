f790df4f40aee9ae39323f6f9f2e28d2c7a03678
kgsi <kuusai1998@gmail.com>
2022-10-24 13:00:51 +0900

    eslintとprettierを追加

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  ...require('prettier-config-smarthr'),
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [],
+  },
+})
