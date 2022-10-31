7d6b7190fab8e7902cac5da69dbf3eef50ca592b
kgsi <kuusai1998@gmail.com>
2022-10-31 13:54:39 +0900

    vercel要設定ファイルの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config'
-import replacePageLink from './plugins/replacePageLink'
+import { defineConfig } from 'astro/config';
+import replacePageLink from './plugins/replacePageLink';
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   markdown: {
     extendDefaultPlugins: true,
-    remarkPlugins: [replacePageLink],
+    remarkPlugins: [replacePageLink]
   },
-})
+  adapter: vercel()
+});
