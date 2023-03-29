e94d708a1ef502dffb9185ed3db78e473f8f09e2
shinichi.kogiso <kuusai1998@gmail.com>
2023-03-29 12:32:17 +0900

    Productデザインを新設、ファイルを整理

--- src/components/skill/SkillChart.tsx
+++ src/components/skill/SkillChart.tsx
@@ -1,69 +1,9 @@
 import { useState, useEffect } from 'react'
 import { Radar } from 'react-chartjs-2'
 import { Chart as ChartJS, RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend } from 'chart.js'
-import skills from '../../skill.json'
+import { skillData, initialGraphsData, initialLevels } from './Skilldata'
 
 ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend)
-
-const initialDatasets = {
-  datasets: [
-    {
-      label: 'スキル',
-      data: [1, 1, 1, 1],
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 2,
-    },
-  ],
-}
-
-const initialGraphsData = [
-  {
-    name: 'コア',
-    labels: ['行政手続きの知識', '給与計算および税制の知識', '既存プロダクトの理解', 'ファシリテーション能力'],
-    ...initialDatasets,
-  },
-  {
-    name: 'UI',
-    labels: [
-      'HTMLの理解',
-      'CSSの理解',
-      'オブジェクトモデリング',
-      'ブラウザの理解',
-      '画面設計能力',
-      'レガシー画面設計能力',
-      'アクセシビリティの知識',
-      'QA能力',
-      'ビジュアルデザインの知識と理解',
-      'デザインツールの知識',
-      'プロトタイピング能力',
-      'ユーザビリティの知識',
-    ],
-    ...initialDatasets,
-  },
-  {
-    name: 'UX',
-    labels: [
-      '支援技術の知識と理解',
-      '全体設計能力',
-      'HCD/UXDの知識と理解 as UXD',
-      'ブラウザの理解',
-      '画面設計能力',
-      'レガシー画面設計能力',
-      'アクセシビリティの知識',
-      'QA能力',
-      'ビジュアルデザインの知識と理解',
-      'デザインツールの知識',
-      'プロトタイピング能力',
-      'ユーザビリティの知識',
-    ],
-    ...initialDatasets,
-  },
-]
-
-const initialLevels = skills.map((item) => {
-  return { label: item.label, level: 1 }
-})
 
 export const SkillChart: React.FC = () => {
   const [levels, setLevels] = useState<{ label: string; level: number }[]>(initialLevels)
@@ -109,7 +49,7 @@ export const SkillChart: React.FC = () => {
   return (
     <>
       <h2>スキル一覧</h2>
-      {skills.map((skill, parentIndex) => (
+      {skillData.map((skill, parentIndex) => (
         <div key={parentIndex}>
           <h3>{skill.label}</h3>
           {skill.description && <p>{skill.description}</p>}
@@ -139,7 +79,7 @@ export const SkillChart: React.FC = () => {
       <h2>スキルグラフ</h2>
       <p>スキル一覧で選択したスキルデータを分野別にグラフ出力しています。</p>
       {graphsData.map((graphData, index) => (
-        <div key={index} style={{ width: '500px', height: '500px' }}>
+        <div key={index} style={{ width: '600px', height: '600px' }}>
           <h3>{graphData.name}</h3>
           <Radar
             data={graphData}
@@ -150,6 +90,16 @@ export const SkillChart: React.FC = () => {
                   min: 0,
                   ticks: {
                     stepSize: 1,
+                  },
+                  angleLines: {
+                    display: false,
+                  },
+                  pointLabels: {
+                    display: true,
+                    callback: (value) => {
+                      const maxLength = 10
+                      return value.length > maxLength ? value.slice(0, maxLength) + '...' : value
+                    },
                   },
                 },
               },
