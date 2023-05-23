fda7a3f3f5bb6ef59eb9a7b2a4080bf5d80a6c18
shinichi.kogiso <kuusai1998@gmail.com>
2023-05-23 13:53:16 +0900

    名前変更、諸々修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
-import { defineConfig } from 'astro/config';
-import compress from 'astro-compress';
-import vercel from '@astrojs/vercel/serverless';
-import replacePageLink from './src/plugins/replacePageLink';
-
-// https://astro.build/config
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import compress from 'astro-compress'
+import vercel from '@astrojs/vercel/serverless'
+import replacePageLink from './src/plugins/replacePageLink'
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     extendDefaultPlugins: true,
-    remarkPlugins: [replacePageLink]
+    remarkPlugins: [replacePageLink],
   },
-  integrations: [compress(), react()],
+  integrations: [compress()],
   output: 'server',
-  adapter: vercel()
-});
+  adapter: vercel(),
+})
